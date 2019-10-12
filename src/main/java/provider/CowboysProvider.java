9dedd6837428d2dae010c5b845a1feab007165e7
Arūnas Fortūnas <arunas.fortunas@gmail.com>
2019-10-12 09:54:51 +0300

    moved cowboys to separate resource file

--- src/main/java/provider/CowboysProvider.java
+++ src/main/java/provider/CowboysProvider.java
@@ -2,37 +2,13 @@ package provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Cowboy;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
 import java.io.IOException;
 
 public class CowboysProvider {
-    String cowboysJson = "[{\n" +
-            "  \"name\": \"John\",\n" +
-            "  \"health\": 10,\n" +
-            "  \"damage\": 1\n" +
-            "},\n" +
-            "{\n" +
-            "\"name\": \"Bill\",\n" +
-            "\"health\": 8,\n" +
-            "\"damage\": 2\n" +
-            "},\n" +
-            "{\n" +
-            "\"name\": \"Sam\",\n" +
-            "\"health\": 10,\n" +
-            "\"damage\": 1\n" +
-            "},\n" +
-            "{\n" +
-            "\"name\": \"Peter\",\n" +
-            "\"health\": 5,\n" +
-            "\"damage\": 3\n" +
-            "},\n" +
-            "{\n" +
-            "\"name\": \"Philip\",\n" +
-            "\"health\": 15,\n" +
-            "\"damage\": 1\n" +
-            "}]";
-
     public final Cowboy[] getCowboys() throws IOException {
-        return new ObjectMapper().readValue(cowboysJson, Cowboy[].class);
+        return new ObjectMapper().readValue(ResourceUtils.getFile("classpath:Cowboys.json"), Cowboy[].class);
     }
 }
